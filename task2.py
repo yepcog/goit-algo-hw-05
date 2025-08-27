fe7f7cfad53a5ee9185c12034e6103432bae335c
yepcog <yep0cog41@gmail.com>
2025-08-27 20:43:46 +0300

    fixed task2: regex pattern is now meeting the task's conditions

--- task2.py
+++ task2.py
@@ -4,13 +4,9 @@ from decimal import Decimal
 from typing import Callable
 
 def generator_numbers(text: str):
-    words = text.split()
-    for word in words:
-        if re.search(r"\d+\.?\,?\d+", word):
-            word = Decimal(word)
-            yield word
-        else:
-            pass
+    numbers = re.findall(r"\ \d+\.\d+\ ", text)
+    for number in numbers:
+        yield Decimal(number)
 
 def sum_profit(text: str, func: Callable):
     sum_profit = Decimal("0.00")
@@ -21,3 +17,8 @@ def sum_profit(text: str, func: Callable):
 text = "The employee's total income consists of several parts: 1000.01 as the main income, supplemented by additional income of 27.45 and 324.00 dollars."
 total_income = sum_profit(text, generator_numbers)
 print(f"Total income: {total_income}")
+
+# additional testing
+other = "20.12 or 12.28 . 333.1  and 2.9  or 0.2 and 2 or4.4 end 5.5."
+other_total = sum_profit(other, generator_numbers)
+print(f"other: {other_total}")
